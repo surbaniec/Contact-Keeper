9a67ce5f1a91125d8ee85c27be58588a447d09f2
surbaniec <radsler1999@gmail.com>
2021-01-04 17:15:41 +0100

    add backend logic, JWT Authentication, hash password, validation etc.

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-//entry point to backend
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
 
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-//look first for enviroment variable
+// Look first for enviroment variable
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
